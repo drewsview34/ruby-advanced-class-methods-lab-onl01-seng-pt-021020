27c00ca06655784652c81162bb2e092b0bd6c741
Andrew Hinojosa <drews_view@hotmail.com>
2020-03-04 08:31:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,13 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-  
-  def initialize
-    self.create
-  end
 
   def self.all
     @@all
   end
   
   def self.create
-    @@all << name
-    puts "#{self.name}"
+    
   end
   
   def save
